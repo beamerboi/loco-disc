bb4598ea0ac0bed5d5b230b971a202ecc415d95b
beamerboi <40015058+beamerboi@users.noreply.github.com>
2019-02-03 03:57:13 +0100

    vr 1.0.1

---
+++ commands/slots.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+module.exports.run = async (client, message, args) => {
+    if (!message.guild.member(client.user).hasPermission("SEND_MESSAGES")) return message.author.send("I don't have permission to Send Messages. Please enable send messages for my role!");
+
+    let slots = ["🍎", "🍌", "🍒", "🍓", "🍈"];
+    let result1 = Math.floor((Math.random() * slots.length));
+    let result2 = Math.floor((Math.random() * slots.length));
+    let result3 = Math.floor((Math.random() * slots.length));
+    let name = message.author.displayName;
+    let aicon = message.author.displayAvatarURL;
+
+    if (slots[result1] === slots[result2] && slots[result3]) {
+        let wEmbed = new Discord.RichEmbed()
+            .setFooter("You Won!", aicon)
+            .setTitle(':slot_machine:Slots:slot_machine:')
+            .addField('Result:', slots[result1] + slots[result2] + slots[result3], true)
+            .setColor("#f4e842");
+        message.channel.send(wEmbed);
+    } else {
+        let embed = new Discord.RichEmbed()
+            .setFooter('You Lost!', aicon)
+            .setTitle(':slot_machine:Slots:slot_machine:')
+            .addField('Result', slots[result1] + slots[result2] + slots[result3], true)
+            .setColor("#f4e842");
+        message.channel.send(embed);
+    }
+
+}
+
+
+exports.conf = {
+    aliases: []
+};
+
+module.exports.help = {
+    name: 'slots',
+    description: 'Slot Machine',
+    usage: 'slots'
+} 
