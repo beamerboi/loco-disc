ef32b5cfde13dc77865539daa68312da0f97041b
Ghassen Jemaî <40015058+beamerboi@users.noreply.github.com>
2019-02-06 06:17:41 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const botconfig = require('./botconfig.json');
 const Discord = require ("discord.js");
 const bot = new Discord.Client ({disableEveryone : true});
 const fs = require("fs");
-const token = process.env.Token
 bot.commands = new Discord.Collection();
-const coins = require ("./coins.json");
+let coins = require ("./coins.json");
+let xp = require ("./xp.json");
+let purple = botconfig.purple;
+let cooldown = new Set();
+let cdseconds = 5;
+
+
 fs.readdir("./commands/", (err, files)=>{
 
     if(err) console.log(err);
@@ -60,8 +65,8 @@ bot.on("message", async message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
 
-
     let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", 'utf8'));
+
     if(!prefixes[message.guild.id]){
         prefixes[message.guild.id] = {
             prefixes : botconfig.prefix
@@ -83,7 +88,7 @@ console.log(`${coinAmt} ; ${baseAmt}`);
         };
         fs.writeFile("./coins.json", JSON.stringify(coins), (err) =>{
             if (err) console.log(err)
-        });
+        if (err) console.log(err)});
         let coinEmbed = new Discord.RichEmbed()
         .setAuthor(message.author.username)
         .setColor("#faff00")
@@ -95,18 +100,53 @@ console.log(`${coinAmt} ; ${baseAmt}`);
         message.channel.send(coinEmbed).then(msg =>{msg.delete(3000)});
     
     }
+let xpAdd = Math.floor(Math.random()* 7) + 8
+   console.log(xpAdd);
+if(!xp[message.author.id]){
+    xp[message.author.id] = {
+        xp : 0,
+        level : 0
+    };
+}
+
+let curxp = xp[message.author.id].xp;
+let curlvl = xp[message.author.id].level;
+let nextlvl = xp[message.author.id].level * 300;
+xp[message.author.id].xp = curxp + xpAdd;
+if(nextlvl <= xp[message.author.id].xp){
+    xp[message.author.id].level=curlvl + 1;
+    let lvlup = new Discord.RichEmbed()
+    .setColor("#b709c4")
+    .addField("Leveled Up to",`level ${curlvl + 1}`);
+    message.channel.send(lvlup).then(msg =>{msg.delete(5000)});
+
+}
+fs.writeFile("./xp.json", JSON.stringify(xp),(err)=>{
+    if(err) console.log(err)
+});
+if(cooldown.has(message.author.id)){
+    message.delete();
+   return message.reply("Stop spamming dude , chill").then(msg =>{msg.delete(5000)});
+}
+if(!message.member.hasPermission("ADMINISTRATOR")){
+    cooldown.add(message.author.id);
+}
 
 
-    let prefix = prefixes[message.guild.id].prefixes;
-console.log(prefix);
-   
+let prefix = prefixes[message.guild.id].prefixes;
+
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
 
-    let commandfile = bot.commands.get(cmd.slice(prefix.length));
-    if (commandfile) commandfile.run(bot,message, args);
-    
+    if(message.content.startsWith(prefix)){
+        let commandfile = bot.commands.get(cmd.slice(prefix.length));
+        if (commandfile) commandfile.run(bot,message, args);
+    }
+    setTimeout(()=> {
+        cooldown.delete(message.author.id)
+    },cdseconds *1000 )
 
-})
-bot.login(token).catch(err => console.log(err));
+});
+
+bot.login(botconfig.token);
