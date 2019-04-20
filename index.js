3d2fcf1ee41812c4df1bbf193e5df82cee7021f1
Ghassen Jemaî <40015058+beamerboi@users.noreply.github.com>
2019-04-20 13:06:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ bot.user.setActivity("/help to see my whole commands", {type:"Playing"});
 bot.on("guildMemberAdd", async member=>{
 console.log(`${member} Joined the server !`);
 
-let welcomeChannel = member.guild.channels.find('name' ,🔃joins-leaves🔃');
+let welcomeChannel = member.guild.channels.find('name' ,'🔃joins-leaves🔃');
 welcomeChannel.send(`welcome ${member}! Have a nice stay !`)
 });
 
